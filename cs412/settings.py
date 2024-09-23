4e07c6087b98d31bf27b12acca529f64a35ec19d
Camille Christie <112962852+camchris@users.noreply.github.com>
2024-09-23 14:31:03 -0400

    New updates for Heroku deployment

--- cs412/settings.py
+++ cs412/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-vioke$@^^*tp&rxs3uyu=wreirk)72vr-c$tsctjvcx+uv3d!d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
